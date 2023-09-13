c2e55985c9eb25f00b701acc9df9548fa0b3d1f7
Khash-Erdene <khasherdenexx@gmail.com>
2023-09-13 13:45:06 +0800

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,25 @@ export const metadata: Metadata = {
     apple: "/apple-touch-icon.png",
   },
   openGraph: {
-    images: "/public/assets/cover.png",
+    title: "Next.js",
+    description: "The React Framework for the Web",
+    url: "https://nextjs.org",
+    siteName: "Next.js",
+    images: [
+      {
+        url: "https://nextjs.org/og.png",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://nextjs.org/og-alt.png",
+        width: 1800,
+        height: 1600,
+        alt: "My custom alt",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
   },
 };
 
