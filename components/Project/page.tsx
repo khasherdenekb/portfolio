1b9b7ace5605b6d26fcdddb2c7c4cab85388eb6b
Khash-Erdene <khasherdeneswe@gmail.com>
2023-10-21 22:54:25 +0800

    adding image

--- components/Project/page.tsx
+++ components/Project/page.tsx
@@ -10,17 +10,14 @@ export default function ProjectPage() {
     {
       title: "BLOG",
       img: "/assets/blogCover.png",
-      link: "https://blogkx.vercel.app",
     },
     {
       title: "NIKE CLONE UI",
       img: "/assets/nikeCover.png",
-      link: "https://nikekx.vercel.app",
     },
     {
       title: "PORTFOLIO",
       img: "/assets/portfolioCover.png",
-      link: "/",
     },
   ];
   return (
@@ -28,20 +25,18 @@ export default function ProjectPage() {
       <div className="grid w-screen max-w-6xl grid-cols-12 grid-rows-2 gap-4 px-10 py-40 xl:px-0">
         {list.map((item, index) => (
           <Card key={index} className="col-span-12 sm:col-span-4 h-fit">
-            <Link target="_blank" href={item.link}>
-              <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-                <p className="font-bold uppercase text-tiny text-violet-700">
-                  {item?.title}
-                </p>
-              </CardHeader>
-              <Image
-                isZoomed={width > 768 ? true : false}
-                removeWrapper
-                alt="Card background"
-                className="z-0 object-cover w-full h-full"
-                src={item.img}
-              />
-            </Link>
+            <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+              <p className="font-bold uppercase text-tiny text-black">
+                {item?.title}
+              </p>
+            </CardHeader>
+            <Image
+              isZoomed={width > 768 ? true : false}
+              removeWrapper
+              alt="Card background"
+              className="z-0 object-cover w-full h-[220px]"
+              src={item.img}
+            />
           </Card>
         ))}
 
@@ -49,36 +44,34 @@ export default function ProjectPage() {
           isFooterBlurred
           className="w-full h-[200px] md:h-[300px] col-span-12 sm:col-span-5"
         >
-          <Link target="_blank" href="https://hoollymernapp.web.app">
-            <CardHeader className="absolute z-10 flex-col items-start top-1">
-              <p className="font-bold uppercase text-tiny text-white/60">
-                FOOD DELIVERY
-              </p>
-            </CardHeader>
-            <Image
-              removeWrapper
-              isZoomed={width > 768 ? true : false}
-              alt="foodCover image"
-              className="z-0 object-fill w-full h-full"
-              src="/assets/foodCover.png"
-            />
-            <CardFooter className="absolute bottom-0 z-10 justify-between bg-white/30 border-t-1 border-zinc-100/50">
-              <div>
-                <p className="text-tiny">Click and view.</p>
-              </div>
-              <Button className="text-tiny" radius="full" size="sm">
-                MERN STACK
-              </Button>
-            </CardFooter>
-          </Link>
+          <CardHeader className="absolute z-10 flex-col items-start top-1">
+            <p className="font-bold uppercase text-tiny text-black">
+              FOOD DELIVERY
+            </p>
+          </CardHeader>
+          <Image
+            removeWrapper
+            isZoomed={width > 768 ? true : false}
+            alt="foodCover image"
+            className="z-0 object-fill w-full h-full"
+            src="/assets/foodCover.png"
+          />
+          <CardFooter className="absolute bottom-0 z-10 justify-between bg-black/40 border-t-1 border-zinc-100/50">
+            <div>
+              <p className="text-tiny">developed 2023</p>
+            </div>
+            <Button className="text-tiny" radius="full" size="sm">
+              MERN STACK
+            </Button>
+          </CardFooter>
         </Card>
         <Card
           isFooterBlurred
           className="w-full h-[250px] md:h-[300px]  col-span-12 sm:col-span-7"
         >
           <CardHeader className="absolute z-10 flex-col items-start top-1">
-            <p className="font-bold uppercase text-tiny text-white/60">
-              Netflix clone
+            <p className="font-bold uppercase text-tiny text-white">
+              Movie app
             </p>
           </CardHeader>
           <Image
@@ -86,12 +79,12 @@ export default function ProjectPage() {
             removeWrapper
             alt="netflix cover image"
             className="z-0 object-fill w-full h-full"
-            src="/assets/netflixCover.jpg"
+            src="/assets/movieCover.png"
           />
           <CardFooter className="absolute bottom-0 z-10 bg-black/40 border-t-1 border-default-600 dark:border-default-100">
             <div className="flex items-center flex-grow gap-2">
               <div className="flex flex-col">
-                <p className="text-tiny text-white/60">Non deploy</p>
+                <p className="text-tiny text-white/60">developed 2023</p>
               </div>
             </div>
             <Button radius="full" size="sm">
